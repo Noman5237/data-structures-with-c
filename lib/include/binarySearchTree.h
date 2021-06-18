aa7827917ef6cc803f5df9b5bf27f5b40fb7a44f
Noman5237 <anonyman637@gmail.com>
2021-06-18 08:51:16 +0600

    Iterative algos operate on tree and recursive algo operate on nodes

--- lib/include/binarySearchTree.h
+++ lib/include/binarySearchTree.h
@@ -9,6 +9,8 @@
 
 #include <dsCommon.h>
 
+/* ============================== BST_NODE ========================= */
+
 typedef struct BST_Node {
 	int data;
 	
@@ -19,43 +21,60 @@ typedef struct BST_Node {
 
 BST_Node *bst_node_create(int data);
 
-int bst_isEmpty(BST_Node *root);
+/* ============================== BS TREE ========================= */
+
+typedef struct BST {
+	BST_Node *root;
+} BST;
+
+BST *bst_create();
+
+/* ============================== RECURSIVE ========================= */
+
+BST_Node *bst_recursiveInsertNode(BST_Node *newNode, BST_Node *root);
 
 void bst_recursivePreOrderTraversal(BST_Node *root);
 
-void bst_preOrderTraversal(BST_Node *root);
-
 void bst_recursiveInOrderTraversal(BST_Node *root);
-
-void bst_inOrderTraversal(BST_Node *root);
 
 void bst_recursivePostOrderTraversal(BST_Node *root);
 
-void bst_postOrderTraversal(BST_Node *root);
-
 BST_Node *bst_recursiveSearch(int data, BST_Node *root);
-
-BST_Node *bst_search(int data, BST_Node *root);
 
 BST_Node *bst_recursiveMaximum(BST_Node *root);
 
-BST_Node *bst_maximum(BST_Node *root);
-
 BST_Node *bst_recursiveMinimum(BST_Node *root);
-
-BST_Node *bst_minimum(BST_Node *root);
 
 BST_Node *bst_recursiveInOrderPredecessor(BST_Node *node);
 
-BST_Node *bst_InOrderPredecessor(BST_Node *node);
-
 BST_Node *bst_recursiveInOrderSuccessor(BST_Node *node);
 
-BST_Node *bst_InOrderSuccessor(BST_Node *node);
+BST_Node *bst_recursiveDeleteNode(BST_Node *node, BST_Node *root);
 
 void bst_recursiveFree(BST_Node *root);
 
-void bst_free(BST_Node *root);
+/* ============================== ITERATIVE ========================= */
 
+void bst_insertNode(BST_Node *newNode, BST *tree);
+
+void bst_preOrderTraversal(BST *tree);
+
+void bst_inOrderTraversal(BST *tree);
+
+void bst_postOrderTraversal(BST *tree);
+
+BST_Node *bst_search(int data, BST *tree);
+
+BST_Node *bst_maximum(BST *tree);
+
+BST_Node *bst_minimum(BST *tree);
+
+BST_Node *bst_InOrderPredecessor(BST *tree);
+
+BST_Node *bst_InOrderSuccessor(BST *tree);
+
+void bst_deleteNode(BST_Node *node, BST *tree);
+
+void bst_free(BST *tree);
 
 #endif //DATASTRUCTURESCOURSE_BINARYSEARCHTREE_H
